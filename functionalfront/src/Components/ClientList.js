e6186a7c65df8395f938d018ba877b8a4ebb66b7
Bruno Robim <bruno.acad2017@gmail.com>
2022-05-15 23:27:09 -0300

    adicionado CORS na API e Fetch.get para o front, faltam update, create e delete.

--- functionalfront/src/Components/ClientList.js
+++ functionalfront/src/Components/ClientList.js
@@ -1,28 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const ClientList = () => {
-    const atividades = [
-        {
-            id: 1,
-            descricao: "primeiro"
-        },
-        {
-            id: 2,
-            descricao: "segundo"
-        }
-    ];
+    const [clientes, setClientes] = useState([]);
 
-    
+    useEffect(() => {
+        axios.get('http://localhost:8080/clients/')
+            .then(res => {
+                console.log(res)
+                setClientes(res.data)
+            })
+            .catch(err => {
+                console.error();
+            })
+    }, []);
 
     console.log("teste de importação");
     return (
         <>
             <div className="">
                 <ul className="list-group m-4">
-                    {atividades.map(ativ => (
-                        <li key={ativ.id} className="list-group-item">
+                    {clientes.map(clientes => (
+                        <li key={clientes.id} className="list-group-item">
                             <div className="row">
-                                <div className="col-sm-10 mt-2 ml-10">{ativ.id} - {ativ.descricao}</div>
+                                <div className="col-sm-10 mt-2 ml-10">{clientes.id} - {clientes.name}</div>
                                 <div className="col-sm-2 mr-2">
                                     <button type="button" className="btn btn-primary m-2">Primary</button>
                                     <button type="button" className="btn btn-danger m-2">Danger</button>
