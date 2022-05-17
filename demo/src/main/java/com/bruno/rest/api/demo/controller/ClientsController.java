3a89d0edb73aa1128eda108e0715208690a68d43
BrunoRobim <50236231+BrunoRobim@users.noreply.github.com>
2022-05-17 18:12:06 -0300

    Update ClientsController.java

--- demo/src/main/java/com/bruno/rest/api/demo/controller/ClientsController.java
+++ demo/src/main/java/com/bruno/rest/api/demo/controller/ClientsController.java
@@ -20,12 +20,14 @@ public class ClientsController {
     }
 
     @GetMapping
+    @CrossOrigin( origins = "http://localhost:3000") // Liberação de CORS para API
     public List<Client> getClients() {
         System.out.println("Bateu uma onda forte, to vendo macado, em cima do poste.");
         return clientRepository.findAll();
     }
 
     @GetMapping("/{id}")
+    @CrossOrigin( origins = "http://localhost:3000") // Liberação de CORS para API
     public Client getClient(@PathVariable Long id) {
 
         return clientRepository.findById(id).orElseThrow(RuntimeException::new);
@@ -33,22 +35,25 @@ public class ClientsController {
 
     //definir o tipo de midia suportado pela requisição
     @PostMapping
+    @CrossOrigin( origins = "http://localhost:3000") // Liberação de CORS para API
     public ResponseEntity createClient(@RequestBody Client client) throws URISyntaxException {
         Client savedClient = clientRepository.save(client);
         return ResponseEntity.created(new URI("/clients/" + savedClient.getId())).body(savedClient);
     }
 
     @PutMapping("/{id}")
+    @CrossOrigin( origins = "http://localhost:3000") // Liberação de CORS para API
     public ResponseEntity updateClient(@PathVariable Long id, @RequestBody Client client) {
         Client currentClient = clientRepository.findById(id).orElseThrow(RuntimeException::new);
         currentClient.setName(client.getName());
         currentClient.setEmail(client.getEmail());
         currentClient = clientRepository.save(client);
-//        clientRepository.deleteById(currentClient.getId()); //Jeitinho brasileiro a partir daqui, cuidado
+        clientRepository.deleteById(currentClient.getId()); //Jeitinho brasileiro a partir daqui, cuidado
         return ResponseEntity.ok(clientRepository.findById(id));
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin( origins = "http://localhost:3000") // Liberação de CORS para API
     public ResponseEntity deleteClient(@PathVariable Long id) {
         clientRepository.deleteById(id);
         return ResponseEntity.ok().build();
